d5615a9a5ed123a44ad72b6f129e333a172ae580
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-09 11:33:11 +0530

    creating user model

---
+++ 04megaBE/src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema, model } from 'mongoose'
+
+const userSchema = new Schema(
+   {
+      watchHistory: [
+         {
+            type: Schema.Types.ObjectId,
+            ref: 'Video',
+         }
+      ],
+      username: {
+         type: String,
+         required: true,
+         unique: true,
+         index: true
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+         lowercase: true,
+      },
+      fullName: {
+         type: String,
+         required: true,
+      },
+      avatar: {
+         type: String, // public url
+         required: true,
+      },
+      coverImage: {
+         type: String, // public url
+         required: true,
+      },
+      password: {
+         type: String,
+         required: true,
+      },
+      refreshToken: {
+         type: String,
+         required: true,
+      },
+
+   }
+   , { timestamps: true });
+
+export const User = model('User', userSchema);
